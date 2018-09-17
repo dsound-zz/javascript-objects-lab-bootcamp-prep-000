fe32ac3018c83cbf14fd24b66bf77a69dc3c14d7
demian sims <demiansims@gmail.com>
2018-09-17 00:21:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var modRecipe = object 
+  var modRecipe = Object.assign({}, obj)
   delete modRecipe[key]
   return modRecipe
 }
