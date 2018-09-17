3a7cf63e53cefa6f24e78994c5f30d89a349a91b
demian sims <demiansims@gmail.com>
2018-09-17 00:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
-  var newRecipeUpdate = object[key] = value
-  return newRecipeUpdate
+ object[key] = value
+  return object
 }
